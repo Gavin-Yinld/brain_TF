a1e3b5702ca3d0cc61e7d76613556dcffee277cb
Gavin-Yinld <38175502+Gavin-Yinld@users.noreply.github.com>
2020-03-22 18:24:44 +0800

    Add files via upload

---
+++ 4b_Co-expression_analysis_bulk_level/code-input/4b_ETRM.coExp.bulk.R
@@ -0,0 +1,21 @@
+options(stringsAsFactors=F)
+load(file="demo_dataset/tpm.Rdata")
+Egr_ETRM <- c("Egr1","Egr2","Zfp281","Klf9","Klf14")
+Exc_marker <- c("Neurod6","Slc17a7","Nrn1")
+inh_marker <- c("Gad1", "Gad2", "Pnoc", "Slc32a1")
+
+ETRM_Exp <- tpm[which(rownames(tpm)%in%c(Egr_ETRM,Exc_marker,inh_marker)),]
+ETRM_Exp <- log2(ETRM_Exp+1)
+library(pheatmap)
+#pdf("Egr.heatmap.pdf")
+my_palette <- colorRampPalette(c("green","white","red"))(100)
+pheatmap(ETRM_Exp,color=my_palette)
+ETRM_Exp <- ETRM_Exp[c("Klf14","Neurod6","Zfp281","Egr2","Klf9","Egr1","Nrn1","Slc17a7","Gad1","Pnoc","Gad2","Slc32a1"),]
+
+pheatmap(cor(t(ETRM_Exp)),cluster_rows = T,cluster_cols = T,display_numbers=F)
+
+
+
+
+
+
