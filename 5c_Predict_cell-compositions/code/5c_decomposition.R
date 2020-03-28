28ab7aa4fe1509864f22a423cf0d8203ca65e122
Gavin-Yinld <38175502+Gavin-Yinld@users.noreply.github.com>
2020-03-28 10:05:41 +0800

    Update 5c_decomposition.R

--- 5c_Predict_cell-compositions/code/5c_decomposition.R
+++ 5c_Predict_cell-compositions/code/5c_decomposition.R
@@ -1,15 +1,26 @@
 options(stringsAsFactors=F)
+
+# Load the demo dataset
 load("demo-dataset/data.for.decomposition.github.Rdata")
+
 library(DWLS) # more details for this package can be found in https://bitbucket.org/yuanlab/dwls
 predicte.proportion <- NULL
 for(sample in colnames(dataBulk)){
 print(sample)
 dir.create(paste0("../output/",sample,".results"))
+
+# Build signature from single-cell data
 Signature<-buildSignatureMatrixMAST(scdata=dataSC,id=cell_type,path=paste0("../output/",sample,".results"),diff.cutoff=0.5,pval.cutoff=0.01)
+
 bulkExp <- dataBulk[,sample]
 names(bulkExp) <- rownames(dataBulk)
+  
+# Filter signature and bulk data to contain the same differentially expressed genes
 tr<-trimData(Signature,bulkExp)
+  
+# Estimate using dampened weighted least squares (deconvolution)
 solDWLS<-solveDampenedWLS(tr$sig,tr$bulk)
+ 
 predicte.proportion <- cbind(predicte.proportion,solDWLS)
 }
 colnames(predicte.proportion) <- colnames(dataBulk)
