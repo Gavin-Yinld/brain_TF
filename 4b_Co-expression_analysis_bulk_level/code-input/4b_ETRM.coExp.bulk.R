48cb8bf934814981be6c694100229b1fc4c432b6
Gavin-Yinld <38175502+Gavin-Yinld@users.noreply.github.com>
2020-03-28 10:27:07 +0800

    Update 4b_ETRM.coExp.bulk.R

--- 4b_Co-expression_analysis_bulk_level/code-input/4b_ETRM.coExp.bulk.R
+++ 4b_Co-expression_analysis_bulk_level/code-input/4b_ETRM.coExp.bulk.R
@@ -7,11 +7,13 @@ inh_marker <- c("Gad1", "Gad2", "Pnoc", "Slc32a1")
 ETRM_Exp <- tpm[which(rownames(tpm)%in%c(Egr_ETRM,Exc_marker,inh_marker)),]
 ETRM_Exp <- log2(ETRM_Exp+1)
 library(pheatmap)
-#pdf("Egr.heatmap.pdf")
 my_palette <- colorRampPalette(c("green","white","red"))(100)
+
+# ETRM expression
 pheatmap(ETRM_Exp,color=my_palette)
 ETRM_Exp <- ETRM_Exp[c("Klf14","Neurod6","Zfp281","Egr2","Klf9","Egr1","Nrn1","Slc17a7","Gad1","Pnoc","Gad2","Slc32a1"),]
 
+# pairwise correlatioin of TFs in ETRM
 pheatmap(cor(t(ETRM_Exp)),cluster_rows = T,cluster_cols = T,display_numbers=F)
 
 
